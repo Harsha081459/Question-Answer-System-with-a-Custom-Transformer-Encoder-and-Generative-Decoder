783d5539e8f5505f09a923496d68889a826acd64
Lohith <Lohith248@users.noreply.github.com>
2026-04-03 20:41:53 +0530

    Initial setup for test_generative_models.sh

---
+++ test_generative_models.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd "/home/sem6/main file"
+Q1="Who created Python?"
+C1="Python was created by Guido van Rossum and first released in 1991. It is a dynamically typed programming language."
+
+Q2="What is the capital of Mars?"
+C2="Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System."
+
+
+
+# #!/bin/bash
+# cd "/home/sem6/main file"
+# Q1="Who created Python?"
+# C1="Python was created by Guido van Rossum and first released in 1991. It is a dynamically typed programming language."
+# 
+# Q2="What is the capital of Mars?"
+# C2="Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System."
+# 
