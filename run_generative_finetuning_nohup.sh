60de3c025dbbcf1bf70f8a38339de601d5a39213
Lohith <Lohith248@users.noreply.github.com>
2026-04-08 10:37:37 +0530

    Add more features to run_generative_finetuning_nohup.sh

--- run_generative_finetuning_nohup.sh
+++ run_generative_finetuning_nohup.sh
@@ -6,6 +6,15 @@ mkdir -p "${LOG_DIR}"
 LOG_FILE="${LOG_DIR}/generative_train_$(date +%Y%m%d_%H%M%S).log"
 
 nohup python generative_finetuning.py \
+  --tokenizer_path checkpoints_pretrain_base_seq256/step_20000 \
+  --pretrain_ckpt checkpoints_pretrain_base_seq256/step_20000/checkpoint.pt \
+  --output_dir checkpoints_generative_qa \
+  --decoder_variant hybrid \
+  --max_input_len 256 \
+  --max_target_len 48 \
+  --train_batch_size 6 \
+  --eval_batch_size 8 \
+  --grad_accum 2 \
 
 
 # #!/usr/bin/env bash
@@ -16,3 +25,12 @@ nohup python generative_finetuning.py \
 # LOG_FILE="${LOG_DIR}/generative_train_$(date +%Y%m%d_%H%M%S).log"
 # 
 # nohup python generative_finetuning.py \
+#   --tokenizer_path checkpoints_pretrain_base_seq256/step_20000 \
+#   --pretrain_ckpt checkpoints_pretrain_base_seq256/step_20000/checkpoint.pt \
+#   --output_dir checkpoints_generative_qa \
+#   --decoder_variant hybrid \
+#   --max_input_len 256 \
+#   --max_target_len 48 \
+#   --train_batch_size 6 \
+#   --eval_batch_size 8 \
+#   --grad_accum 2 \
